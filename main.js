c339ae9415ac795309b20be1c623aa375a229597
Santi <santiago.gonzalezagudelo@gmail.com>
2023-03-12 23:44:37 -0500

    Finished

--- main.js
+++ main.js
@@ -2,25 +2,14 @@ let tiempo=document.getElementById('ingresoTiempo')
 let inicio=document.getElementById('botonInicio')
 let retorno=document.getElementById('retornoTemp')
 
-cuentaRegresiva(tiempo.value)
-
-
 function cuentaRegresiva(cuenta){
     if(cuenta==0){
-        return cuenta
-    }else{
-        retorno.innerHTML=cuenta
-        return setTimeout(cuentaRegresiva,1000,(cuenta-1))
-    }
-
-}
-
-function cuentaRegresiva(cuenta){
-    if(cuenta==0){
+        retorno.innerHTML='Terminado :D'
+        tiempo.value=''
         return cuenta
     }else{
         console.log(cuenta)
-        return cuentaRegresiva(cuenta-1)
-        
+        retorno.innerText=`${cuenta}`
+        return setTimeout(cuentaRegresiva,1000,(cuenta-1))
     }
 }
